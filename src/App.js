f852f20e8605dbed33ed675820ee2963ceb29a1e
Timothy Garvin <timothygarvin4@gmail.com>
2023-06-12 11:18:14 -0700

    Create Squares assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import GenerateBox from './components/box-gen';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Create Squares</h1>
+      <GenerateBox></GenerateBox>
     </div>
   );
 }
---
+++ src/components/box-gen.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+
+const GenerateBox = (props) => {
+    const [color, setColor] = useState('')
+    const [arrColors, setArrColors] = useState([])    
+
+    const handleColor = (e) =>{
+        e.preventDefault();
+        setArrColors([...arrColors, color])
+    }
+
+    const BoxStyle = {
+        height: '75px',
+        width: '75px',
+        margin: '10px',
+        outline: 'solid 1px black',
+        backgroundColor: `${color}`,
+    }
+
+    const ConStyle = {
+        display: 'flex',
+        alignContent: 'center'
+    }
+
+
+    return(
+        <div>
+            <form onSubmit = {handleColor}>
+                <div>
+                    <label>Color: </label>
+                    <input type='text' onChange = {(e) => setColor(e.target.value)}/>
+                    <div></div>
+                    <input type='submit' value='Add Square!'/>
+                </div>
+            </form>
+            {arrColors.map((box, index)=>{
+                return (
+                    <div style={ConStyle}>
+                        <div key={index} style={
+                            {
+                                height: '75px',
+                                width: '75px',
+                                margin: '10px',
+                                outline: 'solid 1px black',
+                                backgroundColor: `${box}`,
+                            }
+                        }></div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+export default GenerateBox
